d3bde6af8c7b1d1c1fd9813f778ba6178c23b0d2
TADASINA SAIVIGNESH REDDY <vigneshreddy693@gmail.com>
2026-01-14 13:12:47 +0530

    Add route for SharedChat component

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ const App = () => (
           <Route path="/auth" element={<Auth />} />
           <Route path="/team" element={<Team />} />
           <Route path="/helmet" element={<HelmetReveal />} />
+          <Route path="/share/:shareId" element={<SharedChat />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
